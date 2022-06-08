bb446a9c912ce8801939742f3fa6f6dae211c4b5
Ethan Zhang <ethanzhang@macbook-air-5.myfiosgateway.com>
2022-06-08 19:36:52 -0400

    Adds profile picture/bio

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import profile_picture from "../assets/profile_picture.png";
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div class="split left">
+          <div className="centered">
+            <img 
+              className="profile_image"
+              src={profile_picture}
+              alt="Profile Pic"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Ethan Zhang</div>
+            <div className="brief_description">
+              I am a rising junior at Columbia University studying Computer Science
+              and Mathematics.
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
